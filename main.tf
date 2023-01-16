d7ca0a280adf6735a576c24f4fce5b170ec1ae1b
Sharifoff <efqanserifov@gmail.com>
2023-01-16 13:25:42 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,9 +17,9 @@ resource "aws_instance" "cerberus" {
   user_data = file("./install-nginx.sh")
 }
 
-resource "aws_key_pair" "cerberus-key" {
-  key_name = "cerberus"
-  public_key = file(".ssh/cerberus.pub")
+resource "aws_key_pair" "keyyy" {
+  key_name = "keyyy"
+  public_key = file(".ssh/id.pub")
 }
 
 
@@ -27,7 +27,7 @@ resource "aws_eip" "eip" {
   vpc      = true
   instance = aws_instance.cerberus.id
   provisioner "local-exec" {
-    command = "echo ${aws_eip.eip.public_dns} >> /root/cerberus_public_dns.txt"
+    command = "echo ${aws_eip.eip.public_dns} >> /home/cloudshell-user/cerberus_public_dns.txt"
   }
 
 }
